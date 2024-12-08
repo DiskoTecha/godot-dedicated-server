29976c350523c33d945e11093fcfed54abdd449c
Dylan Wadsworth <dylanwadsworth@gmail.com>
2024-12-08 12:48:23 -0700

    Add token expiration based on time added to game server

--- server_test/PlayerVerification.gd
+++ server_test/PlayerVerification.gd
@@ -2,26 +2,19 @@ extends Node
 
 var awaiting_verification = {}
 
+@onready var awaiting_verification_expiration_timer = Timer.new()
+
+func _ready():
+	awaiting_verification_expiration_timer.autostart = true
+	awaiting_verification_expiration_timer.wait_time = 10
+	awaiting_verification_expiration_timer.timeout.connect(_on_awaiting_verification_expiration_timer_timeout)
+	self.add_child(awaiting_verification_expiration_timer)
+
+
 func start(player_id: String):
 	awaiting_verification[player_id] = {"Timestamp": Time.get_unix_time_from_system()}
 	GameServer.fetchToken(int(player_id))
 	print(awaiting_verification)
-
-
-#func verify(player_id: String, token: String):
-	#var token_verified = false
-	#while Time.get_unix_time_from_system() - awaiting_verification[player_id].Timestamp <= 30:
-		#if GameServers.expected_game_tokens.has(token):
-			#GameServers.expected_game_tokens.erase(token)
-			#token_verified = true
-			#createPlayerContainer(player_id)
-			#awaiting_verification.erase(player_id)
-		#else:
-			#await get_tree().create_timer(2).timeout
-	#GameServer.returnTokenVerificationResults(token_verified)
-	#if not token_verified:
-		#awaiting_verification.erase(player_id)
-		#multiplayer.multiplayer_peer.disconnect_peer(int(player_id))
 
 
 func verify(player_id: String, token: String):
@@ -41,6 +34,25 @@ func verify(player_id: String, token: String):
 	if not token_verified:
 		awaiting_verification.erase(player_id)
 		multiplayer.multiplayer_peer.disconnect_peer(int(player_id))
+
+
+func _on_awaiting_verification_expiration_timer_timeout():
+	var current_time = Time.get_unix_time_from_system()
+	var start_time
+	if awaiting_verification == {}:
+		pass
+	else:
+		for key in awaiting_verification.keys():
+			start_time = awaiting_verification[key].Timestamp
+			if current_time - start_time >= 30:
+				awaiting_verification.erase(key)
+				var connected_peers = multiplayer.get_peers()
+				if connected_peers.has(key):
+					GameServer.returnTokenVerificationResults(key, false)
+					GameServer.multiplayer.multiplayer_peer.disconnect_peer(key)
+	print("Awaiting verification")
+	print(awaiting_verification)
+
 
 func createPlayerContainer(player_id):
 	pass
